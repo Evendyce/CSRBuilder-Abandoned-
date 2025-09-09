b0c3aeecbbdbad3e9903ecd193d7a9d2a51e8593
Evendyce <don.min.max@gmail.com>
2025-09-09 12:38:16 +0200

    Implement CSR XML builder

--- src/Csr.XmlBuilder/JsonLoader.cs
+++ src/Csr.XmlBuilder/JsonLoader.cs
@@ -1,12 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.Json;
 
-namespace Csr.XmlBuilder
+namespace Csr.XmlBuilder;
+
+public static class JsonLoader
 {
-    internal class JsonLoader
+    public static CsrExportBundle FromFile(string path)
     {
+        var json = File.ReadAllText(path);
+        return FromJson(json);
+    }
+
+    public static CsrExportBundle FromJson(string json)
+    {
+        using var doc = JsonDocument.Parse(json);
+        var root = doc.RootElement;
+
+        var bundle = new CsrExportBundle
+        {
+            CruiseName = root.GetProperty("Core").GetProperty("CruiseName").GetString() ?? string.Empty,
+            CsrLocalId = root.GetProperty("Core").GetProperty("CsrLocalId").GetString(),
+            Abstract = root.GetProperty("Core").GetProperty("Abstract").GetString(),
+            RevisionDateUtc = TryGetDateTime(root.GetProperty("Core"), "RevisionDateUtc"),
+            BeginUtc = TryGetDateTime(root.GetProperty("Temporal"), "BeginUtc"),
+            EndUtc = TryGetDateTime(root.GetProperty("Temporal"), "EndUtc"),
+            West = TryGetDouble(root.GetProperty("Spatial"), "West"),
+            East = TryGetDouble(root.GetProperty("Spatial"), "East"),
+            South = TryGetDouble(root.GetProperty("Spatial"), "South"),
+            North = TryGetDouble(root.GetProperty("Spatial"), "North"),
+            ResponsibleLab = ParseOrganisation(root.GetProperty("Parties"), "ResponsibleLab"),
+            SeaAreas = ParseSeaAreas(root.GetProperty("Spatial")),
+            Moorings = ParseMoorings(root)
+        };
+
+        return bundle;
+    }
+
+    static DateTime? TryGetDateTime(JsonElement parent, string name)
+    {
+        if (parent.TryGetProperty(name, out var el) && el.ValueKind == JsonValueKind.String)
+        {
+            if (DateTime.TryParse(el.GetString(), CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var dt))
+                return dt;
+        }
+        return null;
+    }
+
+    static double? TryGetDouble(JsonElement parent, string name)
+    {
+        if (parent.TryGetProperty(name, out var el) && el.TryGetDouble(out var d))
+            return d;
+        return null;
+    }
+
+    static Organization? ParseOrganisation(JsonElement parent, string name)
+    {
+        if (!parent.TryGetProperty(name, out var orgEl) || orgEl.ValueKind != JsonValueKind.Object)
+            return null;
+
+        var org = new Organization
+        {
+            Name = orgEl.GetProperty("Name").GetString() ?? string.Empty,
+            Url = orgEl.TryGetProperty("Url", out var urlEl) && urlEl.ValueKind == JsonValueKind.String ? urlEl.GetString() : null,
+            Country = orgEl.TryGetProperty("Country", out var cEl) && cEl.ValueKind == JsonValueKind.String ? cEl.GetString() : null
+        };
+        return org;
+    }
+
+    static List<Keyword> ParseSeaAreas(JsonElement spatial)
+    {
+        var list = new List<Keyword>();
+        if (spatial.TryGetProperty("SeaAreas", out var arr) && arr.ValueKind == JsonValueKind.Array)
+        {
+            foreach (var el in arr.EnumerateArray())
+            {
+                list.Add(new Keyword
+                {
+                    Code = el.TryGetProperty("Code", out var cEl) ? cEl.GetString() : null,
+                    Label = el.TryGetProperty("Label", out var lEl) ? lEl.GetString() : null
+                });
+            }
+        }
+        return list;
+    }
+
+    static List<Mooring> ParseMoorings(JsonElement root)
+    {
+        var list = new List<Mooring>();
+        if (root.TryGetProperty("Moorings", out var mo) &&
+            mo.TryGetProperty("Rows", out var rows) && rows.ValueKind == JsonValueKind.Array)
+        {
+            foreach (var r in rows.EnumerateArray())
+            {
+                list.Add(new Mooring
+                {
+                    DataCategoryCode = r.TryGetProperty("DataCategoryCode", out var dc) ? dc.GetString() : null,
+                    DataCategoryLabel = r.TryGetProperty("DataCategoryLabel", out var dl) ? dl.GetString() : null,
+                    Description = r.TryGetProperty("Description", out var desc) ? desc.GetString() : null,
+                    Latitude = r.TryGetProperty("Latitude", out var lat) && lat.TryGetDouble(out var la) ? la : (double?)null,
+                    Longitude = r.TryGetProperty("Longitude", out var lon) && lon.TryGetDouble(out var lo) ? lo : (double?)null,
+                    EventTimeUtc = TryGetDateTime(r, "EventTimeUtc"),
+                    PrincipalInvestigator = r.TryGetProperty("PrincipalInvestigator", out var pi) ? pi.GetString() : null,
+                    OrganizationName = r.TryGetProperty("OrganisationName", out var on) ? on.GetString() : null,
+                    Email = r.TryGetProperty("Email", out var em) ? em.GetString() : null,
+                    PlatformDescription = r.TryGetProperty("PlatformDescription", out var pd) ? pd.GetString() : null
+                });
+            }
+        }
+        return list;
     }
 }
--- src/Csr.XmlBuilder/Ns.cs
+++ src/Csr.XmlBuilder/Ns.cs
@@ -10,14 +10,13 @@ namespace Csr.XmlBuilder
     public static class Ns
     {
         public static readonly XNamespace gmd = "http://www.isotc211.org/2005/gmd";
-        public static readonly XNamespace gmi = "http://www.isotc211.org/2005/gmi";
-        public static readonly XNamespace srv = "http://www.isotc211.org/2005/srv";
         public static readonly XNamespace gco = "http://www.isotc211.org/2005/gco";
-        public static readonly XNamespace gts = "http://www.isotc211.org/2005/gts";
+        public static readonly XNamespace gmi = "http://www.isotc211.org/2005/gmi";
+        public static readonly XNamespace gml = "http://www.opengis.net/gml/3.2";
         public static readonly XNamespace gmx = "http://www.isotc211.org/2005/gmx";
+        public static readonly XNamespace gts = "http://www.isotc211.org/2005/gts";
         public static readonly XNamespace xlink = "http://www.w3.org/1999/xlink";
-        public static readonly XNamespace sdn = "http://www.seadatanet.org";
-        public static readonly XNamespace gml = "http://www.opengis.net/gml";
         public static readonly XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+        public static readonly XNamespace sdn = "https://www.seadatanet.org/urnschema";
     }
 }
